59addddd4fe15f96e384a323dc3394bbb9a7a6ca
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2026-04-15 08:26:48 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//*****************************************************************************
+//** 2515. Shortest Distance to Target String in a Circular Array   leetcode **
+//*****************************************************************************
+
+int closestTarget(char** words, int wordsSize, char* target, int startIndex) {
+    int minDist = INT_MAX;
+
+    for (int i = 0; i < wordsSize; i++) {
+        if (strcmp(words[i], target) == 0) {
+            int forward = (i - startIndex + wordsSize) % wordsSize;
+            int backward = (startIndex - i + wordsSize) % wordsSize;
+            
+            int dist = forward < backward ? forward : backward;
+            
+            if (dist < minDist) {
+                minDist = dist;
+            }
+        }
+    }
+
+    return (minDist == INT_MAX) ? -1 : minDist;
+}
